f083a2af4bc7cca10961136f6f1309e10a445beb
79265 <jiw1984@gmail.com>
2022-08-02 23:22:08 +0300

    Тест git 1

--- src/main/java/ru/job4j/calculator/TestGit.java
+++ src/main/java/ru/job4j/calculator/TestGit.java
@@ -5,6 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         int word = 2;
         int war = 4;
-        System.out.println(word + war);
+        System.out.println(word + war + 5);
     }
 }
