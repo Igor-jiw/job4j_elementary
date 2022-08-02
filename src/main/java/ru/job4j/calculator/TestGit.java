d9641303f6eecfa4b900de5fc6a2abba4961a259
79265 <jiw1984@gmail.com>
2022-08-02 23:12:29 +0300

    Тест git 1

---
+++ src/main/java/ru/job4j/calculator/TestGit.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+public class TestGit {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    public static void main(String[] args) {
+        int word = 2;
+        System.out.println(word);
+    }
+}
